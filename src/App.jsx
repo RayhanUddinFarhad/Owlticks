7303abcbc2acb34af3e38356a84e4113b3e76e1f
ITP <RayhanUddinFarhad11@gmail.com>
2023-09-11 16:56:35 +0600

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Footer from './components/shared/Footer'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   )
 }
